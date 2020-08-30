7e4de03aac8ffb7ca27c1402ece86b4f429fb7f8
Hamza Alam <halam@twitter.com>
2020-08-30 16:16:37 -0400

    Adding my site stuff

--- tutorial0_serve/main.js
+++ tutorial0_serve/main.js
@@ -1,1 +1,8 @@
-console.log("hello!")
+function greeting(name){
+    return "Hello, my name is " + name
+}
+
+let whoIsThis = greeting("Hamza") + '!'
+
+// this function will return a greeting for the name "Hamza"
+console.log(whoIsThis)
