4fc03855c6457f289774ae2a9971a1f01a8d727a
Hamza Alam <halam@twitter.com>
2020-12-22 16:41:22 -0500

    resize svg

--- Project 1/main.js
+++ Project 1/main.js
@@ -1,5 +1,5 @@
 let margin = { top: 0, right: 0, bottom: 0, left: 0 },
-	width = 1100 - margin.left - margin.right,
+	width = 900 - margin.left - margin.right,
 	height = 900 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
